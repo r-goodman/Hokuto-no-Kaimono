cbbd3ff6b938df10e82bdfbc00fe0369dc0037df
r-goodman <r.goodman@dundee.ac.uk>
2012-11-28 20:48:10 +0000

    Attempt 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,26 @@ class ApplicationController < ActionController::Base
   	end
   	@current_basket
   end
+
+  def current_artist(artist)
+    if user_signed_in?
+
+      if current_user.artist.nil?
+
+        redirect_to root_url, :alert => "You do not have the permissions to access that page"
+
+      else
+
+        if current_user.artist.id == artist.id
+          true
+        else
+          redirect_to root_url, :alert => "You do not have the permissions to access that page"
+        end
+
+      end
+
+    else
+      redirect_to new_user_session_path, :warning => "Please sign in to continue"
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,27 +25,4 @@ module ApplicationHelper
 		@tenAlbums = Album.order(:release_date).limit(10).all
 		return @tenAlbums
 	end
-
-	def current_artist(artist)
-		if user_signed_in?
-
-			if current_user.artist.nil?
-
-				redirect_to root_url, :alert => "You do not have the permissions to access that page"
-
-			else
-
-				if current_user.artist.id == artist.id
-					true
-				else
-					redirect_to root_url, :alert => "You do not have the permissions to access that page"
-				end
-
-			end
-
-		else
-			redirect_to new_user_session_path, :warning => "Please sign in to continue"
-		end
-	end
-
 end
